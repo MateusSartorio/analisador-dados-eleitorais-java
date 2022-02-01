b5d67b1e28e51a43b153d1c4e3b2c1380d63900a
MateusSartorio <mateussartorio39@gmail.com>
2022-01-31 22:05:26 -0300

    second_commit

--- src/App.java
+++ src/App.java
@@ -1,4 +1,7 @@
 import java.io.*;
+import java.text.DecimalFormat;
+import java.time.LocalDate;
+import java.time.Period;
 import java.util.*;
 
 import javax.swing.plaf.synth.SynthSeparatorUI;
@@ -6,14 +9,14 @@ import javax.swing.plaf.synth.SynthSeparatorUI;
 public class App {
     public static void main(String[] args) {
 
-        Map<Integer,Partido> listaPartidos = lePartidos("./testes/" + args[0] + "/in/partidos.csv");
+        Map<Integer,Partido> mapPartidos = lePartidos("./testes/" + args[0] + "/in/partidos.csv");
 
-        List<Candidato> listaCandidatos = leCandidatos("./testes/" + args[0] + "/in/candidatos.csv", listaPartidos);
+        List<Candidato> listaCandidatos = leCandidatos("./testes/" + args[0] + "/in/candidatos.csv", mapPartidos);
 
-        geraRelatorio(listaCandidatos);
+        geraRelatorio(listaCandidatos, mapPartidos);
         
         
-        //imprimeLista(listaPartidos.get(90).getLista_candidatos());
+        //imprimeLista(mapPartidos.get(90).getLista_candidatos());
         // imprimeLista(listaCandidatos);
 
         
@@ -21,7 +24,7 @@ public class App {
 
     static Map<Integer,Partido> lePartidos(String destino) {
 
-        Map<Integer,Partido> listaPartidos = new HashMap<Integer,Partido>();
+        Map<Integer,Partido> mapPartidos = new HashMap<Integer,Partido>();
 
         try(FileInputStream arquivo = new FileInputStream(destino)) {
             InputStreamReader arquivoR = new InputStreamReader(arquivo, "UTF-8");
@@ -40,9 +43,9 @@ public class App {
                 String sigla_partido = data[3];
 
                 Partido partido = new Partido(numero_partido, votos_legenda, nome_partido, sigla_partido);
-                listaPartidos.put(numero_partido, partido);
+                mapPartidos.put(numero_partido, partido);
             }
-            //Collections.sort(listaPartidos);
+            //Collections.sort(mapPartidos);
 
         }
         catch (FileNotFoundException e) {
@@ -54,10 +57,10 @@ public class App {
             System.exit(1);
         }
 
-        return listaPartidos;
+        return mapPartidos;
     }
 
-    static List<Candidato> leCandidatos(String destino, Map<Integer,Partido> listaPartidos) {
+    static List<Candidato> leCandidatos(String destino, Map<Integer,Partido> mapPartidos) {
 
         List<Candidato> listaCandidatos = new LinkedList<Candidato>();
 
@@ -83,11 +86,19 @@ public class App {
                 int numero_partido = Integer.parseInt(data[8]);
 
                 Candidato candidato = new Candidato(numero, votos_nominais, situacao, nome, nome_urna, sexo, data_nasc, destino_voto, numero_partido);
-                listaPartidos.get(numero_partido).adicionaCandidato(candidato);
+                mapPartidos.get(numero_partido).adicionaCandidato(candidato);
+                if(destino_voto.equals("Válido")){
+                    mapPartidos.get(numero_partido).addVotos_nominais(votos_nominais);
+                }
                 listaCandidatos.add(candidato);
+
+                if(situacao.equals("Eleito")) {
+                    mapPartidos.get(numero_partido).addCandidato_eleitos();
+                }
 
             }
             Collections.sort(listaCandidatos);
+            // Collections.sort(mapPartidos.keySet());
         }
         catch (FileNotFoundException e) {
             System.out.println("Arquivo nao encontrado");
@@ -116,7 +127,7 @@ public class App {
         }
     }
 
-    static void geraRelatorio(List<Candidato> listaCandidatos) {
+    static void geraRelatorio(List<Candidato> listaCandidatos, Map<Integer,Partido> mapPartido) {
 
         int numeroVagas = 0;
         for(Candidato temp: listaCandidatos) {
@@ -161,11 +172,87 @@ public class App {
         imprimeMapa(lista3);
         System.out.println();
 
-        System.out.println("Eleitos, que se beneficiaram do sistema proporcional:");
+        System.out.println("Eleitos, que se beneficiaram do sistema proporcional:\n(com sua posição no ranking de mais votados)");
         imprimeMapa(lista4);
 
-        
+        List<Partido> listaPartidos = new LinkedList<Partido>();
+
+        for(int j : mapPartido.keySet())
+            listaPartidos.add(mapPartido.get(j));
+
+        System.out.println("\nVotação dos partidos e número de candidatos eleitos:");
+        imprimeRelatorio6(listaPartidos);
+
+        System.out.println("\nVotação dos partidos (apenas votos de legenda):");
+        imprimeRelatorio7(listaPartidos);
+
+        System.out.println("\nPrimeiro e último colocados de cada partido:");
+        imprimeRelatorio8(listaPartidos);
+
+        int q1 = 0, q2 = 0, q3 = 0, q4 = 0;
+        Candidato c = listaCandidatos.get(0);
+        // for(Candidato c: listaCandidatos) {
+
+            String[] dataAtual = new Date().toString().split(" ");
+            String[] dataNascimento = c.getData_nasc().toString().split(" ");
+            
+            for(String s: dataAtual)
+                System.out.print(s + ", ");
+            
+            System.out.println();
+
+            for(String s: dataNascimento)
+                System.out.print(s + ", ");
+            
+            System.out.println();
+
+            // System.out.println(dataAtual);
+            // System.out.println(dataNascimento);
+        // }
 
 
+    }
+
+    static void imprimeRelatorio6(List<Partido> listaPartidos) {
+        Collections.sort(listaPartidos);
+        int i = 1;
+        for(Partido p: listaPartidos)
+            System.out.println(i++ + " - " + p + (p.getVotos_nominais() + p.getVotos_legenda()) + " votos (" + p.getVotos_nominais() + " nominais e " + p.getVotos_legenda() + " de legenda), " + p.getCandidatos_eleitos() + " candidatos eleitos");
+    }
+
+    static void imprimeRelatorio7(List<Partido> lista) {
+        Collections.sort(lista, new Partido.ComparadorVotoLegenda()); 
+
+        int i = 1;
+        String numero;
+        for(Partido p: lista) {
+            DecimalFormat numberFormat = new DecimalFormat("###.##");
+
+            numero = numberFormat.format( 100 * p.getVotos_legenda() / (double) (p.getVotos_legenda() + p.getVotos_nominais())) + "% do total do partido";
+            if(numero.contains("NaN")){
+                numero = "proporção não calculada, 0 voto no partido";
+            }   
+
+            System.out.println(i++ + " - " + p + p.getVotos_legenda() + " votos de legenda (" + numero + ")");
+        }
+            
+    }
+
+    static void imprimeRelatorio8(List<Partido> lista) {
+        Collections.sort(lista,new Partido.ComparadorMaisVotado());
+        int i = 1;
+        for(Partido p: lista) {
+            List<Candidato> tempList = p.getLista_candidatos();
+            Collections.sort(tempList);
+            Candidato primeiro = tempList.get(0);
+            Candidato ultimo = tempList.get(tempList.size() - 1);
+
+            if(primeiro.getDestino_voto().equals("Válido") || ultimo.getDestino_voto().equals("Válido")) {
+                System.out.print(i++ + " - " + p);
+                System.out.print(primeiro.getNome_urna() + " (" + primeiro.getNumero() + ", " + primeiro.getVotos_nominais() + " votos) / ");
+                System.out.println(ultimo.getNome_urna() + " (" + ultimo.getNumero() + ", " + ultimo.getVotos_nominais() + " votos)");
+            }
+                   
+        }
     }
 }
--- src/Partido.java
+++ src/Partido.java
@@ -1,7 +1,8 @@
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
-public class Partido {
+public class Partido implements Comparable<Partido> {
     private int numero_partido;
     private int votos_legenda;
     private String nome_Partido;
@@ -9,6 +10,7 @@ public class Partido {
     private List<Candidato> lista_candidatos;
 	private int votos_nominais_totais;
 	private int candidatos_eleitos;
+	private Candidato candidato_mais_votado;
 
     public Partido(int numero_partido, int votos_legenda, String nome_Partido, String sigla_partido) {
 		this.numero_partido = numero_partido;
@@ -18,6 +20,15 @@ public class Partido {
 		this.lista_candidatos = new ArrayList<Candidato>();
 		this.votos_nominais_totais = 0;
 		this.candidatos_eleitos = 0;
+		this.setCandidato_mais_votado(null);
+	}
+
+	public Candidato getCandidato_mais_votado() {
+		return candidato_mais_votado;
+	}
+
+	public void setCandidato_mais_votado(Candidato candidato_mais_votado) {
+		this.candidato_mais_votado = candidato_mais_votado;
 	}
 
 	public int getNumero_partido() {
@@ -62,6 +73,21 @@ public class Partido {
     public void adicionaCandidato(Candidato c){
         this.lista_candidatos.add(c);
         c.setPartido(this);
+
+		if(this.candidato_mais_votado == null) {
+			this.candidato_mais_votado = c;
+		}
+		else if(c.getVotos_nominais() > this.candidato_mais_votado.getVotos_nominais()) {
+			this.candidato_mais_votado = c;
+		}
+		else if(c.getVotos_nominais() == this.candidato_mais_votado.getVotos_nominais()) {
+			if(c.getNumero() < this.candidato_mais_votado.getNumero()) {
+				this.candidato_mais_votado = c;
+			}
+			else if(c.getData_nasc().compareTo(this.candidato_mais_votado.getData_nasc()) < 0) {
+				this.candidato_mais_votado = c;
+			}
+		}
     }
 
     public void removeCandidato(Candidato c){
@@ -86,7 +112,52 @@ public class Partido {
 
     @Override
     public String toString(){
-        return this.sigla_partido.toUpperCase() + " - " + this.numero_partido + ", ";
+        return this.sigla_partido + " - " + this.numero_partido + ", ";
     }
+
+	public int compareTo(Partido p){
+		int diff = (p.getVotos_nominais()+p.getVotos_legenda())-(this.votos_nominais_totais+this.votos_legenda);
+        if (diff == 0){
+            if(p.getNumero_partido() < this.numero_partido){
+                return 1;
+            }else{
+                return -1;
+            }
+        }
+        return diff;
+	}
+
+	public static class ComparadorVotoLegenda implements Comparator<Partido>{
+		public int compare(Partido p1, Partido p2){
+			int diff1 = p2.votos_legenda - p1.votos_legenda;
+			if (diff1 == 0){
+				int diff2 = p2.votos_nominais_totais - p1.votos_nominais_totais;
+				if(diff2 == 0){
+					if(p2.numero_partido < p1.numero_partido){
+						return 1;
+					}else{
+						return -1;
+					}
+				}
+				return diff2;
+			}
+			
+			return diff1;
+		}
+	}
+
+	public static class ComparadorMaisVotado implements Comparator<Partido>{
+		public int compare(Partido p1, Partido p2){
+			int diff = p2.getCandidato_mais_votado().getVotos_nominais() - p1.getCandidato_mais_votado().getVotos_nominais();
+				if(diff == 0){
+					if (p2.getCandidato_mais_votado().getData_nasc().compareTo(p1.getCandidato_mais_votado().getData_nasc()) > 0){
+						return 1;
+					}else{
+						return -1;
+					}
+				}
+			return diff;
+		}
+	}
 
 }
